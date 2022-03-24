d6debe0940e04a3abab4326b57d23b1d2532c19e
Flo <77297251+Flo-2@users.noreply.github.com>
2022-03-24 14:53:59 +0100

    commit on 'master

--- script.js
+++ script.js
@@ -3,12 +3,24 @@ const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
 
 let apiQuotes = [];
 
+// Show loading
+function loading() {
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+// Hide Loading
+function complete() {
+    quoteContainer.hidden = false;
+    loader.hidden = true;
+}
 // Show New Quote
 function newQuote() {
-   
+   loading();
     //Pick a random quote from apiQuotes array
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
    
@@ -27,11 +39,15 @@ function newQuote() {
         quoteText.classList.remove('long-quote');
     }
     console.log (quote);
-    quoteText.textContent = quote.text;  
+
+    // Set Quote, Hide Loader
+    quoteText.textContent = quote.text; 
+    complete();
 }
 
 // Get Quotes From API
 async function getQuotes() {
+    loading();
     const apiUrl = 'https://type.fit/api/quotes';
     
 
